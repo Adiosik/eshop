059ec76f8a98e422bf7cc35d97c0e39969945ae1
Daniel <simansky.dan@gmail.com>
2024-02-19 09:09:51 +0100

    Changed images names

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Article() {
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img src="http://satyr.io/100/1" className="card-img-top" alt="image 1" />
+            <img src="/images/stenova-lampa.jpg" className="card-img-top" alt="image 1" />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
