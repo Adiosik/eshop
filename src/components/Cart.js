9a3b4887d25f3300fc2bdba3e9d555c281b6b175
Daniel <simansky.dan@gmail.com>
2024-02-19 13:36:16 +0100

    Added total and empty cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 export default function Cart({ cartItems }) {
+    const getTotalPrice = () => {
+        return cartItems.reduce((total, item) => total + item.price, 0);
+    };
+    
+    if (cartItems.length === 0) {
+        return (
+            <div>
+                <h2>Cart</h2>
+                <p>Add something to your cart, make it from your heart.</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h2>Cart</h2>
@@ -11,6 +24,7 @@ export default function Cart({ cartItems }) {
                     </li>
                 ))}
             </ul>
+            <p>Total: ${getTotalPrice()}</p>
         </div>
     )
 }
