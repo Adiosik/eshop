52ef7e64d1a7098c486a60842a86e9cf48b12df1
Daniel <simansky.dan@gmail.com>
2024-02-20 11:47:47 +0100

    CartItem modified

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function CartItem({ item }) {
-    return (
-        <li>
-            {item.title} - €{item.price}
-        </li>
-    );
+  return (
+    <ul className="list-group">
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <span>{item.title} €{item.price}</span>
+        <button className="btn btn-outline-primary btn-sm">Remove</button>
+      </li>
+    </ul>
+  );
 }
