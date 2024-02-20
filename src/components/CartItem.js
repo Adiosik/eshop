30d9728c76a3575d1dc0bed5c0923f6cbf49800d
Daniel <simansky.dan@gmail.com>
2024-02-20 14:48:21 +0100

    Modified cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,7 +4,8 @@ export default function CartItem({ item }) {
   return (
         <ul className="list-group">
             <li className="list-group-item d-flex justify-content-between align-items-center">
-                <span>{item.title} €{item.price}</span>
+                <span>{item.title}</span>
+                <span>€{item.price}</span>
                 <button className="btn btn-outline-primary btn-sm">Remove</button>
             </li>
         </ul>
