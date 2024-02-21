39186d7ce517c94c5ee2079a59e9331c30a49805
Daniel <simansky.dan@gmail.com>
2024-02-21 09:15:11 +0100

    Removed imports

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import data from "./data"
 import Cart from "./components/Cart"
 import CartItem from "./components/CartItem"
 import ArticleList from "./components/ArticleList"
-import Article from "./components/Article"
-import EmailInput from "./components/EmailInput"
 
 export default function App() {
     const [cartItems, setCartItems] = React.useState([]);
