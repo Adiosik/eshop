d3b78d017e319767c54599877bedfc7d80671d9f
Daniel <simansky.dan@gmail.com>
2024-02-22 10:10:19 +0100

    Update emailInput

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -4,11 +4,11 @@ export default function EmailInput() {
     return (
         <form>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="example@example.com" aria-describedby="emailHelp" />
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label for="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="example@example.com" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <button type="submit" class="btn btn-primary">Place order</button>
+            <button type="submit" id="submitBtn" className="btn btn-primary btn-lg">Place order</button>
         </form>
     )
 }
