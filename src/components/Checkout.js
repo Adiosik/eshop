f7c9b457b081b3c92492bc1bb2a4abceaffc7061
Daniel <simansky.dan@gmail.com>
2024-02-22 14:28:24 +0100

    Deleted emailHelp

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -8,7 +8,6 @@ export default function Checkout() {
             <div class="my-4">
                 <label for="exampleInputEmail1" className="form-label">Email address</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" placeholder="example@example.com" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <button type="submit" id="submitBtn" className="btn btn-primary btn-lg">Place order</button>
         </form>
