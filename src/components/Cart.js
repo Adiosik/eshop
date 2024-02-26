d728d40db9125da3d111c424dc262c9dd438fe5e
Daniel <simansky.dan@gmail.com>
2024-02-26 16:05:50 +0100

    Refactoring of returns

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,10 +16,14 @@ export default function Cart({ cartItems, handleRemoveFromCart }) {
         setOrderSent(true);
     }
 
-    // Podmíněné vykreslení zprávy o odeslání objednávky nebo seznamu položek v košíku a formuláře pro e-mail
     return (
         <div>
-            {/* Podmínka pro zobrazení objednávky odeslané */}
+            {cartItems.length === 0 && !orderSent && (
+                <div>
+                    <p className="h2">Cart</p>
+                    <p>Add something to your cart, make it from your heart.</p>
+                </div>
+            )}
             {orderSent && (
                 <div>
                     <p className="alert alert-success">Your order was sent. Thank you. You can buy more if you want now.</p>
@@ -27,33 +31,25 @@ export default function Cart({ cartItems, handleRemoveFromCart }) {
                     <p>Add something to your cart, make it from your heart.</p>
                 </div>
             )}
-
-            {/* Podmínka pro zobrazení košíku */}
-            {!orderSent && (
+            {!orderSent && cartItems.length > 0 && (
                 <div>
                     <p className="h2">Cart</p>
-                    {/* Podmínka pro prázdný košík */}
-                    {cartItems.length === 0 ? (
-                        <p>Add something to your cart, make it from your heart.</p>
-                    ) : (
-                        <ul className="list-group">
-                            {/* Vykreslení seznamu položek v košíku pomocí komponenty CartItem */}
-                            {cartItems.map((item, index) => (
-                                <CartItem 
-                                    key={index} 
-                                    item={item} 
-                                    handleRemoveFromCart={handleRemoveFromCart}
-                                />
-                            ))}
-                            <li className="list-group-item d-flex justify-content-between align-items-center">Total: ${getTotalPrice()}</li>
-                        </ul>
-                    )}
-
-                    {/* Podmínka pro zobrazení formuláře pro e-mail a tlačítka Checkout */}
-                    {!showEmailInput ? (
-                        <button onClick={() => setShowEmailInput(true)} className="btn btn-primary btn-lg mt-3">Checkout</button>
-                    ) : (
+                    <ul className="list-group">
+                        {/* Vykreslení seznamu položek v košíku pomocí komponenty CartItem */}
+                        {cartItems.map((item, index) => (
+                            <CartItem 
+                                key={index} 
+                                item={item} 
+                                handleRemoveFromCart={handleRemoveFromCart}
+                            />
+                        ))}
+                        <li className="list-group-item d-flex justify-content-between align-items-center">Total: ${getTotalPrice()}</li>
+                    </ul>
+                    {/* Tlačítko "Checkout" zobrazí formulář pro e-mail a Place order */}
+                    {showEmailInput ? (
                         <Checkout onSubmit={onSubmit} />
+                    ) : (
+                        <button onClick={() => setShowEmailInput(true)} className="btn btn-primary btn-lg mt-3">Checkout</button>
                     )}    
                 </div>
             )}
