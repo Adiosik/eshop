8ccde2334a1f1e963bfca64716a092c340621fb3
Daniel <simansky.dan@gmail.com>
2024-02-29 16:11:17 +0100

    Changed to ternary operator

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -31,15 +31,13 @@ export default function Cart({ cartItems, handleRemoveFromCart, setCartItems }) 
                     <p className="alert alert-success">Your order was sent. Thank you.<br />You can buy more if you want now.</p>
                 </section>
             )}
-            {cartItems.length === 0 && (
+            <h2>Cart</h2>
+            {cartItems.length === 0 ? (
                 <section>
-                    <h2>Cart</h2>
                     <p>Add something to your cart, make it from your heart.</p>
                 </section>
-            )}
-            {cartItems.length > 0 && (
+            ) : (
                 <section>
-                    <h2>Cart</h2>
                     <ul className="list-group">
                         {/* Vykreslení seznamu položek v košíku pomocí komponenty CartItem */}
                         {cartItems.map((item, index) => (
