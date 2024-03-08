b45c156225427c5143c81c42d1210096b13945b4
Daniel <simansky.dan@gmail.com>
2024-03-08 10:47:10 +0100

    Deleted footer from card

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Article({ item, handleAddToCart, isInCart, isCheckoutLoading }) {
     return (
-        <article className="card h-100">
+        <article className="card h-100 d-flex flex-column">
             <div className="ratio ratio-4x3">
                 <img
                     src={item.thumbnail}
@@ -14,7 +14,7 @@ export default function Article({ item, handleAddToCart, isInCart, isCheckoutLoa
                 <h3 className="card-title">{item.title}</h3>
                 <p className="card-text">{item.description}</p>
             </div>
-            <div className="card-footer d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-center p-3">
                 <p className="mb-0">€{item.price}</p>
                 <button
                     onClick={() => handleAddToCart(item)}
