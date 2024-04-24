270f7a4a6de4e183dca1f15824c3645d8e64516d
Daniel <simansky.dan@gmail.com>
2024-04-24 14:02:42 +0200

    Modified article

--- src/components/Article.js
+++ src/components/Article.js
@@ -21,20 +21,21 @@ export default function Article({ item }) {
             <div className="card-body">
                 <div className="d-flex align-items-center gap-2">
                     <StarRating rating={item.rating} />
-                    <span className="card-text">{item.rating}</span>
+                    <span className="card-text fw-bold">{item.rating}</span>
+                    <p></p>
                 </div>
                 <h3 className="card-title">{item.title}</h3>
                 <p className="card-text">{item.description}</p>
             </div>
             <div className="
                 card-footer 
-                d-flex justify-content-between align-items-center bg-transparent 
+                d-flex justify-content-between align-items-end bg-transparent 
                 border-top-0 pb-3 gap-3"
             >
                 <div className="d-flex flex-column">
                     <p className="card-text">Stock: {remainingStock}</p>
                     <p className="mb-0 text-decoration-line-through">€{item.price}</p>
-                    <p className="mb-0 text-primary fw-bold">€{calculateDiscountedPrice(item.price, item.discountPercentage)}</p>
+                    <p className="mb-0 text-primary fs-3 fw-bold">€{calculateDiscountedPrice(item.price, item.discountPercentage)}</p>
                 </div>
                 {remainingStock > 0 ? (
                     <button
