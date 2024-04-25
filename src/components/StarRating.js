96646ef2ccaa7d38533881e4d455d4cd43466138
Daniel <simansky.dan@gmail.com>
2024-04-25 09:03:15 +0200

    Refactoring

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,22 +4,14 @@ import "./StarRating.css";
 export default function StarRating({ rating }) {
     const percentFilled = (rating / 5) * 100;
 
+    const stars = Array.from({ length: 5 }).map((_, index) => (
+        <span key={index} className="star">&#x2605;</span>
+    ));
+
     return (
         <div className="star-rating">
-            <div className="stars-empty">
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-            </div>
-            <div className="stars-full" style={{ width: `${percentFilled}%` }}>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-                <span className="star">&#x2605;</span>
-            </div>
+            <div className="stars-empty">{stars}</div>
+            <div className="stars-full" style={{ width: `${percentFilled}%` }}>{stars}</div>
         </div>
     );
 }
