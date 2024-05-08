420d1d8c98fdca7b9326a71ac6a09c6e6df6561e
Daniel <simansky.dan@gmail.com>
2024-05-08 11:13:14 +0200

    When there is one image then it is set without the carousel

--- src/components/ArticleCarousel.js
+++ src/components/ArticleCarousel.js
@@ -9,16 +9,26 @@ export default function ArticleCarousel({ images }) {
     }
 
     return (
-        <Carousel interval={null} className="carousel-dark" activeIndex={index} onSelect={handleSelect}>
-            {images.map((image, idx) => (
-                <Carousel.Item className="ratio ratio-4x3" key={idx}>
-                    <img
-                        className="card-img-top d-block object-fit-cover"
-                        src={image}
-                        alt=""
-                    />
-                </Carousel.Item>
-            ))}
-        </Carousel>
+        images.length > 1 ? (
+            <Carousel interval={null} className="carousel-dark" activeIndex={index} onSelect={handleSelect}>
+                {images.map((image, idx) => (
+                    <Carousel.Item className="ratio ratio-4x3" key={idx}>
+                        <img
+                            className="card-img-top d-block object-fit-cover"
+                            src={image}
+                            alt=""
+                        />
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        ) : (
+            images[0] ? (
+                <img
+                    className="card-img-top d-block object-fit-cover"
+                    src={images[0]}
+                    alt=""
+                />
+            ) : null
+        )
     )
 }
