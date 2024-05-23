68659a37a781275e20d25a193a751632584ba271
Daniel <simansky.dan@gmail.com>
2024-05-23 14:37:06 +0200

    Added message for adding 3 characters

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,8 @@ import React from "react";
 import Product from "./Product";
 import { formatCategoryName } from "../utilities";
 
-export default function ProductList({ products, loadMore, isLoadingData, isMaxProductsLoaded, isProductsFound, selectedCategory }) {
+export default function ProductList({ products, loadMore, isLoadingData, isMaxProductsLoaded, isProductsFound, selectedCategory, inputValue }) {
+
     return (
         <div className="col mb-5 mt-4">
             {selectedCategory && (
@@ -10,40 +11,46 @@ export default function ProductList({ products, loadMore, isLoadingData, isMaxPr
                     <h2 className="h1">{formatCategoryName(selectedCategory)}</h2>
                 </div>
             )}
-            {isProductsFound ? (
+            {inputValue.length > 0 && inputValue.length < 3 ? (
+                <div className="alert alert-warning" role="alert">
+                    Please enter at least 3 characters to search.
+                </div>
+            ) : (
                 <>
-                    <h2 className="visually-hidden">Products</h2>
-                    <div className="row row-gap-4 align-content-stretch">
-                        {products.map((item) => (
-                            <div key={item.id} className="col-12 col-sm-6 col-lg-4 col-xxl-3">
-                                <Product
-                                    item={item}
-                                />
+                    {isProductsFound && products.length > 0 ? (
+                        <>
+                            <h2 className="visually-hidden">Products</h2>
+                            <div className="row row-gap-4 align-content-stretch">
+                                {products.map((item) => (
+                                    <div key={item.id} className="col-12 col-sm-6 col-lg-4 col-xxl-3">
+                                        <Product item={item} />
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    {!isMaxProductsLoaded && (
-                        <div className="text-center mt-5">
-                            {isLoadingData ? (
-                                <div className="alert alert-info" role="alert">
-                                    Searching products
+                            {!isMaxProductsLoaded && (
+                                <div className="text-center mt-5">
+                                    {isLoadingData ? (
+                                        <div className="alert alert-info" role="alert">
+                                            Searching products
+                                        </div>
+                                    ) : (
+                                        <button
+                                            className="btn btn-outline-primary btn-lg"
+                                            onClick={loadMore}
+                                            disabled={isLoadingData}
+                                        >
+                                            Load more
+                                        </button>
+                                    )}
                                 </div>
-                            ) : (
-                                <button
-                                    className="btn btn-outline-primary btn-lg"
-                                    onClick={loadMore}
-                                    disabled={isLoadingData}
-                                >
-                                    Load more
-                                </button>
                             )}
+                        </>
+                    ) : (
+                        <div className="alert alert-warning" role="alert">
+                            No products found.
                         </div>
                     )}
                 </>
-            ) : (
-                <div className="alert alert-warning" role="alert">
-                    No products found.
-                </div>
             )}
         </div>
     );
